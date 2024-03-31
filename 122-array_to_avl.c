d6a3e9b6919471e6f4907d557951810dd7671c6e
dirarkh <dirarkhebri@gmail.com>
2024-03-31 14:42:53 +0100

    AVL

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,20 +1,25 @@
 #include "binary_trees.h"
 
+
 /**
  * array_to_avl - builds an AVL tree from an array
- * @array: array to create from
- * @size: size of the array
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
  *
- * Return: a pointer to the root node of the created AVL tree
- *         NULL on failure
+ * Return: pointer to the created node, or NULL on failure
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-	unsigned int i;
-	avl_t *root = NULL;
+	avl_t *tree = NULL;
+	size_t i;
 
-	for (i = 0; i < size; i++)
-		avl_insert(&root, array[i]);
+	if (!array || size < 1)
+		return (NULL);
 
-	return (root);
+	avl_insert(&tree, array[0]);
+
+	for (i = 1; i < size; i++)
+		avl_insert(&tree, array[i]);
+
+	return (tree);
 }
--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,41 +1,48 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
+ * binary_tree_height_B - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
  *
- * Return: the balance factor
- *         0 if tree is NULL
+ * Return: Height or 0 if tree is NULL
  */
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height_B(const binary_tree_t *tree)
 {
-	int l_height, r_height;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
 	if (!tree)
 		return (0);
 
-	l_height = tree->left ? (int)binary_tree_height(tree->left) : -1;
-	r_height = tree->right ? (int)binary_tree_height(tree->right) : -1;
+	if (tree->left)
+		height_l = 1 + binary_tree_height_B(tree->left);
 
-	return (l_height - r_height);
+	if (tree->right)
+		height_r = 1 + binary_tree_height_B(tree->right);
+
+	if (height_l > height_r)
+		return (height_l);
+	return (height_r);
 }
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: tree to measure the height of
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
  *
- * Return: height of the tree
- *         0 if tree is NULL
+ * Return: Balance factor or 0
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t l_height = 0, r_height = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
 	if (!tree)
 		return (0);
+
 	if (tree->left)
-		l_height = 1 + binary_tree_height(tree->left);
+		height_l = 1 + binary_tree_height_B(tree->left);
 	if (tree->right)
-		r_height = 1 + binary_tree_height(tree->right);
-	return ((l_height > r_height) ? l_height : r_height);
+		height_r = 1 + binary_tree_height_B(tree->right);
+
+	return (height_l - height_r);
 }
